593b3b1b88fc18cfa5ff629f3f0a68cacfa32914
李依林 <liyilin16@mails.tsinghua.edu.cn>
2019-02-19 10:22:12 +0800

    Modify mnist loading again

--- mnist/load_mnist.py
+++ mnist/load_mnist.py
@@ -31,16 +31,16 @@ class Data:
 
 def load_mnist_whole(scale, shift, PATH = '.'):
     print ('fetch MNIST dataset')
-    #mnist = fetch_mldata('MNIST original', data_home=PATH)
-    #mnist.data = mnist.data.astype(np.float32)*scale + shift
-    #mnist.target = mnist.target.astype(np.int32)
-    #whole = Data(mnist.data, mnist.target)
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x = np.concatenate((x_train, x_test))
-    y = np.concatenate((y_train, y_test))
-    x = x.astype(np.float32) * scale + shift
-    y = y.astype(np.int32)
-    whole = Data(x, y)
+    mnist = fetch_mldata('MNIST original', data_home=PATH)
+    mnist.data = mnist.data.astype(np.float32)*scale + shift
+    mnist.target = mnist.target.astype(np.int32)
+    whole = Data(mnist.data, mnist.target)
+    #(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    #x = np.concatenate((x_train, x_test))
+    #y = np.concatenate((y_train, y_test))
+    #x = x.astype(np.float32) * scale + shift
+    #y = y.astype(np.int32)
+    #whole = Data(x, y)
 
     print ("load mnist done", whole.data.shape)
     return whole
