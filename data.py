8e2394e8a5ae12a71f838b0e61fa38859359fd96
李依林 <liyilin16@mails.tsinghua.edu.cn>
2019-02-19 15:10:10 +0800

    Refactor loading

---
+++ data.py
@@ -0,0 +1,25 @@
+import numpy as np
+from chainer import cuda
+
+class Data:
+    def __init__(self, data, label):
+        assert len(data) == len(label)
+        self.data = data
+        self.label = label
+        self.index = np.arange(len(data))
+        print(cupy.get_array_module(self.label))
+        print(cupy.get_array_module(cuda.to_gpu(self.label)))
+
+    def get_index_data(self, index_list):
+        return cuda.to_gpu(self.data[index_list])
+
+    def get(self, n, need_index = False):
+        ind = np.random.permutation(self.data.shape[0])
+        if need_index:
+            return cuda.to_gpu(self.data[ind[:n],:].astype(np.float32)), \
+                cuda.to_gpu(self.label[ind[:n]].astype(np.int32)), \
+                self.index[ind[:n]].astype(np.int32)
+        else:
+            return cuda.to_gpu(self.data[ind[:n],:].astype(np.float32)), \
+                cuda.to_gpu(self.label[ind[:n]].astype(np.int32))
+
--- mnist/load_mnist.py
+++ mnist/load_mnist.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from ..data import Data 
 import sys
 try:
     import cPickle as pickle
@@ -9,26 +10,7 @@ import numpy as np
 from chainer import cuda
 import os
 import scipy.io as scio
-cupy = cuda.cupy
 
-class Data:
-    def __init__(self, data, label):
-        assert len(data) == len(label)
-        self.data = data
-        self.label = label
-        self.index = np.arange(len(data))
-        print(cupy.get_array_module(self.label))
-        print(cupy.get_array_module(cuda.to_gpu(self.label)))
-
-    def get_index_data(self, index_list):
-        return cuda.to_gpu(self.data[index_list])
-
-    def get(self, n, need_index = False):
-        ind = np.random.permutation(self.data.shape[0])
-        if need_index:
-            return cuda.to_gpu(self.data[ind[:n],:].astype(np.float32)), cuda.to_gpu(self.label[ind[:n]].astype(np.int32)), self.index[ind[:n]].astype(np.int32)
-        else:
-            return cuda.to_gpu(self.data[ind[:n],:].astype(np.float32)), cuda.to_gpu(self.label[ind[:n]].astype(np.int32))
 
 def load_mnist_whole(scale, shift, PATH = '.'):
     print ('fetch MNIST dataset')
